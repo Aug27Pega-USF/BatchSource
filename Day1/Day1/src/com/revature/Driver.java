d9bd745730db9e8a6afe3ba96119af91de32b70d
luisriveradoi <luisriveradoi@gmail.com>
2018-08-27 15:43:31 -0400

    added Day1 Java project

---
+++ Day1/Day1/src/com/revature/Driver.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class Driver {
+
+	// JVM looks for a method with this signature as an entrance point to the program
+	public static void main(String[] args) {	// main method
+		// void - no return type
+		// public - makes it accessible throughout
+		// static - means that the variable or function is shared between  
+		// all instances of that class as it belongs to the type, not the actual objects themselves. 
+		System.out.println("Roll Tide");
+	}
+}
