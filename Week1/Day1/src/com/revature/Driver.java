db78d901c275371e58244bbc6e88ec55ec4e51bb
sqtruong <sqtruong18@gmail.com>
2018-08-27 14:39:03 -0500

    added week 1 day 1 example

---
+++ Week1/Day1/src/com/revature/Driver.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class Driver {
+	//JVM looks for a method with this signature as an entrance point to the program
+	public static void main (String[] args) {
+		System.out.println("Roll Tide");
+	}
+}
