228203ce3b8c0471db6a79050d8f8a93c719b234
Zachary Rudolph <zachjrudolph@gmail.com>
2018-08-27 15:44:20 -0400

    My Eclipse project

---
+++ Week1/DayOne/src/com/revature/driver.java
@@ -0,0 +1,9 @@
+package com.revature;
+
+public class driver {
+	//method signature
+	//starting point of the of executiong that the JVM looks for specifically
+	public static void main(String args[]) {
+		System.out.println("Hi");
+	}
+}
