84f466f9f2aa2d1a05d70bac7c6b31178ef165aa
Kevin <kmedara2015@outlook.com>
2018-08-29 11:09:27 -0400

    Added Partially completed First assignment

--- Week One/PegaDayOne/src/com/revature/Driver.java
+++ Week One/PegaDayOne/src/com/revature/Driver.java
@@ -1,8 +1,18 @@
 package com.revature;
 
+import com.revature.beans.Person;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Roll Tide");
+		Person person1 = new Person();
+		
+		person1.setName("Kevin");
+		Person Person2 = new Person("Jeff", 23, 180);
+		
+//		System.out.println(person1.getName() + " is from " + person1.getHomePlanet());
+		
 	}
 }
+	
---
+++ Week One/PegaDayOne/src/com/revature/test/StaticImportTest.java
@@ -0,0 +1,13 @@
+package com.revature.test;
+
+import static com.revature.beans.Person.*;
+
+public class StaticImportTest {
+
+	public static void main(String[] args) {
+		
+		testPrint();// method from person class
+
+	}
+
+}
