dc828237e3b9eedf394aed190c0eaedf6994f6aa
unknown <jsullivan0593@gmail.com>
2018-09-05 12:21:10 -0400

    added register logic

--- BankingProject/src/main/java/support/LoginLogic.java
+++ BankingProject/src/main/java/support/LoginLogic.java
@@ -1,21 +1,39 @@
 package support;
 
+import java.util.Scanner;
+
 public class LoginLogic {
 	//Scanner scan = new Scanner(System.in);
-	public static User register() {
+	/*
+	 * register method will only be used to register new Customers
+	 * user type will be automatically set to "Customer" 
+	 */
+	public User register() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Creating new Account...");
 		User newUser = new User();
 		System.out.println("Please Enter Your First Name:");
-		newUser.setFname("Lois");
+		//newUser.setFname("Lois");
+		String fname = scan.next();
+		newUser.setFname(fname);
 		System.out.println("Please Enter Your Last Name:");
-		newUser.setLname("Pewterschmidt");
+		//newUser.setLname("Pewterschmidt");
+		String lname = scan.next();
+		newUser.setLname(lname);
 		System.out.println("Please Enter Your Username:");
-		newUser.setUname("LooseLoisKiss79");
+		//newUser.setUname("LooseLoisKiss79");
+		String uname = scan.next();
+		newUser.setUname(uname);
 		System.out.println("Please enter a password:");
-		newUser.setPw("password3");
+		//newUser.setPw("password3");
+		String pw = scan.next();
+		newUser.setPw(pw);
 		System.out.println("Lastly, please enter a starting balance:");
-		newUser.setC_acct(5035.75);
-		
+		//newUser.setC_acct(5035.75);
+		double cacct = scan.nextDouble();
+		newUser.setC_acct(cacct);
+		newUser.setType("Customer");
+		scan.close();
 		return newUser;
 	}
 }
--- BankingProject/src/main/java/support/TheBank.java
+++ BankingProject/src/main/java/support/TheBank.java
@@ -9,18 +9,20 @@ import java.util.Arrays;
 public class TheBank {
 
 	static Storage s = new Storage();
+	static LoginLogic l = new LoginLogic();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String path = "banking_storage.txt";
 		File file = new File(path);
-		User jon = new User("Jon", "Sully", "js93", "password1", "customer", "2020009", 75000.57,
-				2000);
-		User deb = new User("Debbie", "Downer", "ddsad", "felineaids", "customer", "20012008", 80000.57,
-				3000);
-		//User lois;
+		User jon = new User("Jon", "Sully", "js93", "password1", "Customer", "2020009", 75000.57,
+				2000.00);
+		User deb = new User("Debbie", "Downer", "ddsad", "felineaids", "Customer", "20012008", 80000.57,
+				3000.00);
+		User lois = l.register();
 		ArrayList<User> list = new ArrayList<User>();
 		list.add(jon);
 		list.add(deb);
+		list.add(lois);
 		//File file = new File("C://Users//HHGregg//Desktop//jsullivan93//BatchSource//BankingProject//bank_storage.txt");
 			//File file = new File("C://Users//HHGregg//Desktop//jsullivan93//BatchSource//BankingProject//src//main//resourses//bank_storage.txt");
 		
