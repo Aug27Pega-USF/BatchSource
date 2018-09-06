b24d77f9a224fb605ddb5e4b80cf7c4f25d649f8
pldl <prestonzlau@gmail.com>
2018-09-06 11:03:40 -0400

    ayy lmao

---
+++ parlor/src/main/java/pizza/parlor/App.java
@@ -0,0 +1,15 @@
+package pizza.parlor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
